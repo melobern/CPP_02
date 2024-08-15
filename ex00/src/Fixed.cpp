e522223ace1820030c0451a0a38fac9f8a76ee5a
Melodie Bernard (from school) <mbernard@z3r8p3.42lyon.fr>
2024-08-15 12:54:02 +0200

    changed private variables name to follow the norm of putting _ before private var

--- ex00/src/Fixed.cpp
+++ ex00/src/Fixed.cpp
@@ -6,14 +6,14 @@
 /*   By: mbernard <mbernard@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/15 10:15:55 by mbernard          #+#    #+#             */
-/*   Updated: 2024/08/15 11:36:45 by mbernard         ###   ########.fr       */
+/*   Updated: 2024/08/15 12:52:10 by mbernard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Fixed.hpp"
 
 Fixed::Fixed() {
-    this->fixedComma = 0;
+    this->_fixedComma = 0;
     std::cout << "Default constructor called" << std::endl;
     return;
 }
@@ -31,19 +31,19 @@ Fixed::~Fixed() {
 
 int     Fixed::getRawBits(void) const {
     std::cout << "getRawBits member function called" << std::endl;
-    return (this->fixedComma);
+    return (this->_fixedComma);
 }
 
 void    Fixed::setRawBits(int const raw) {
     std::cout << "setRawBits member function called" << std::endl;
-    this->fixedComma = raw;
+    this->_fixedComma = raw;
     return;
 }
 
 Fixed   &Fixed::operator=(const Fixed &num) {
     std::cout << "Copy assignment operator called" << std::endl;
     if (this != &num) {
-        this->fixedComma = num.getRawBits();
+        this->_fixedComma = num.getRawBits();
     }
     return *this;
 }
